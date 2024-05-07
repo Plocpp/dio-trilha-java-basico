3978568e6abf697cc85c9feab3fdddf28fd05625
Jefferson De Souza Ramos <103444740+Plocpp@users.noreply.github.com>
2024-05-06 23:17:12 -0300

    sincronizando metodos 06/04

--- tipos-variaveis/src/Metodos/Metodos.java
+++ tipos-variaveis/src/Metodos/Metodos.java
@@ -1,9 +1,44 @@
 package Metodos;
 
 public class Metodos {
-    public static void main(String[] args) {
+    public double somar(int num1, int num2){
+        //LOGICA - FINALIDADE DO MÈTODO
+
+        return ... ;
+    }
+    public void imprimir(String texto){
+        //LOGICA - FINALIDADE DO METODO
+        //AQUI NÃO PRECISA DE RETURN
+        //POIS NÃO SERÁ RETORNADO NENHUM RESULTADO
+    }
+    //throws Exception : indica que o metodo ao ser utilizado 
+    //poderá gerar um exceção
+    public double dividir(int dividendo, int divisor)throws Exception{
        
-        
+    } 
+    //este metodo não pode ser visto por outras classes no projeto
+    private void metodoPrivado(){}
+
+    //alguns equivocos estruturais
+    public void validar(){}
+
+    //este metodo deveria retornar algum valor
+    //no caso boolean (true ou false)
+
+    public void calcularEnviar(){
+        //um metodo deve representar uma nuica responsabilidade
     }
     
-}
+    public void gravarCliente(String nome, String  cpf, Integer telefone,... )
+    //este metodos tem a intenção de gravar 
+    //informações de um cliente , por que não criar
+    // objeto cliente e passar como paramentro
+    //veja aqui
+
+public void gravarCliente(Cliente cliente){}
+//ou
+public void gravar(Cliente cliente){}
+} 
+
+    
+
