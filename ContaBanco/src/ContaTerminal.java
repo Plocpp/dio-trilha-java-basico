d7df0be2bc6c1f8fa0e1aedb14944b3a36e029d6
Jefferson De Souza Ramos <103444740+Plocpp@users.noreply.github.com>
2024-05-12 22:06:04 -0300

    meu projeto conta banco 1.1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //TODO Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso cliente usuario
+        //Obter pela scanner os valores digitados no terminal
+        //Exibir a mensagem da conta criada
         
+       // Utilizei try , pois o programa estava dando erro na palavra scanner,
+       // pois não estava fechando corretamente,o try resolveu isso fechando o codigo
         try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
             System.out.println("Por favor digite o número da Agência ! ");
             int numero = scanner.nextInt();
@@ -21,11 +27,14 @@ public class ContaTerminal {
             
             System.out.println("Por favor Digite o Saldo em Conta !");
             Double saldo = scanner.nextDouble();
-            
+
             System.out.println("Olá Cliente "+nomeCliente+",");
             System.out.println("obrigado por criar uma  \n conta em nosso Banco, ");
             System.out.println("sua agência é: "+agencia+" conta: "+numero+", ");
             System.out.println("seu saldo R$"+saldo+" já está Disponivel para Saque");
+
+            
+            // tendo entendido o projeto, fiz esse codigo, espero ter realizado o desafio com exito!!!
         }
     }
 }
